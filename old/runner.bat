66c5ab2c91d6eab09876d2805b7d8241754f9e45
reguluspercussion <260179483+reguluspercussion@users.noreply.github.com>
2026-02-13 00:29:41 +0900

    Rename runner.bat to old/runner.bat

---
+++ old/runner.bat
@@ -0,0 +1,6 @@
+@echo off
+REM 作業ディレクトリを強制指定
+cd /d "C:\Users\regu-\OneDrive\デスクトップ\discord-automate"
+
+REM python 実体をフルパスで指定
+C:\Users\regu-\AppData\Local\Microsoft\WindowsApps\python.exe "C:\Users\regu-\OneDrive\デスクトップ\discord-automate\runner.py"
